5a955f8d637d84dfed36431f806ed578bc2dc052
Maximilian Werzinger <maximilian.werzinger@googlemail.com>
2018-12-05 14:39:23 +0100

    add Functions: - generatePom - generateDirectory

---
+++ src/main/java/Packaging.java
@@ -0,0 +1,21 @@
+
+
+public enum Packaging {
+	WAR("war"),
+	JAR("jar"),
+	EJB("ejb"),
+	EAR("ear"),
+	POM("pom"),
+	MAVENPLUGIN("maven-plugin");
+
+	private String value;
+
+	private Packaging(String value) {
+		this.value = value;
+	}
+
+	public String value() {
+		return value;
+	}
+
+}
---
+++ src/main/java/Pom.java
@@ -0,0 +1,80 @@
+
+
+public class Pom {
+
+	private String modelVersion;
+	private String groupId;
+	private String artifactId;
+	private String version;
+	private String packaging;
+	private String name;
+	private String description;
+
+	public Pom(String modelVersion, String groupId, String artifactId, String version, String packaging, String name,
+			String description) {
+		this.modelVersion = modelVersion;
+		this.groupId = groupId;
+		this.artifactId = artifactId;
+		this.version = version;
+		this.packaging = packaging;
+		this.name = name;
+		this.description = description;
+	}
+
+	public String getModelVersion() {
+		return modelVersion;
+	}
+
+	public void setModelVersion(String modelVersion) {
+		this.modelVersion = modelVersion;
+	}
+
+	public String getGroupId() {
+		return groupId;
+	}
+
+	public void setGroupId(String groupId) {
+		this.groupId = groupId;
+	}
+
+	public String getArtifactId() {
+		return artifactId;
+	}
+
+	public void setArtifactId(String artifactId) {
+		this.artifactId = artifactId;
+	}
+
+	public String getVersion() {
+		return version;
+	}
+
+	public void setVersion(String version) {
+		this.version = version;
+	}
+
+	public String getPackaging() {
+		return packaging;
+	}
+
+	public void setPackaging(String packaging) {
+		this.packaging = packaging;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+}
