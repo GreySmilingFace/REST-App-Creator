e672b4700ef4582595d32cb525acdb5bcf479256
Maximilian Werzinger <maximilian.werzinger@googlemail.com>
2018-12-11 15:40:11 +0100

    add Entities: EntityAttribute, SimpleEntity | add generate .java-files

--- src/main/java/de/greySmilingFace/TestClass.java
+++ src/main/java/de/greySmilingFace/TestClass.java
@@ -1,11 +1,14 @@
 package de.greySmilingFace;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import de.greySmilingFace.Creator.PomCreator;
 import de.greySmilingFace.Creator.ProjectGenerator;
 import de.greySmilingFace.Object.Packaging;
 import de.greySmilingFace.Object.Pom;
+import de.greySmilingFace.Object.SimpleEntity;
 
 public class TestClass {
 
@@ -13,14 +16,19 @@ public class TestClass {
 
 		System.out.println("Start generating...");
 
-		String workspaceDireytory = "C:\\Users\\Max\\Desktop\\TestOrdner";
+		List<SimpleEntity> list = new ArrayList();
+		list.add(new SimpleEntity("test1", null));
+		list.add(new SimpleEntity("test2", null));
+		list.add(new SimpleEntity("test3", null));
+
+		String workspaceDireytory = "C:\\Users\\B069448\\Desktop\\TestOrdner";
 		String projectName = "testProject";
 
 		ProjectGenerator generator = new ProjectGenerator(new PomCreator(new Pom("4.0.0", "de.nuerberger.test",
 				"testProject", "0.0.1-SNAPSHOT", Packaging.WAR.value(), "test", "this is a test")));
 
 		try {
-			generator.generateProject(workspaceDireytory, projectName);
+			generator.generateProject(workspaceDireytory, projectName, list);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
